69b97ae7114b4bf76709c5cff588f02e2e36ab96
kimberly <92147193+pgm-kim@users.noreply.github.com>
2022-06-11 22:49:36 +0200

    extra js

--- index.js
+++ index.js
@@ -89,3 +89,181 @@ btnText.innerHTML = "show less";
 moreText.style.display = "inline";
 }
 }
+
+const portfolio = {
+  init() {
+
+    this.on();
+    this.off();
+
+    this.onposter();
+    this.offposter();
+
+    this.onproject();
+    this.offproject();
+  },
+
+   on() {
+    document.getElementById("overlay").style.display = "block";
+  },
+  
+  off() {
+    document.getElementById("overlay").style.display = "none";
+  },
+
+
+   onposter() {
+    document.getElementById("overlaypp").style.display = "block";
+  },
+  
+   offposter() {
+    document.getElementById("overlaypp").style.display = "none";
+  },
+
+
+   onproject() {
+    document.getElementById("overlayproject").style.display = "block";
+  },
+  
+  offproject() {
+    document.getElementById("overlayproject").style.display = "none";
+  },
+
+};
+
+portfolio.init();
+
+
+
+
+
+
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
+
+function onposter() {
+  document.getElementById("overlaypp").style.display = "block";
+}
+
+function offposter() {
+  document.getElementById("overlaypp").style.display = "none";
+}
+
+function onproject() {
+  document.getElementById("overlayproject").style.display = "block";
+}
+
+function offproject() {
+  document.getElementById("overlayproject").style.display = "none";
+}
+
+
+function myFunction() {
+var dots = document.getElementById("dots");
+var moreText = document.getElementById("more");
+var btnText = document.getElementById("btnprojects");
+
+if (dots.style.display === "none") {
+dots.style.display = "inline";
+btnText.innerHTML = "show more"; 
+moreText.style.display = "none";
+} else {
+dots.style.display = "none";
+btnText.innerHTML = "show less"; 
+moreText.style.display = "inline";
+}
+}
+
+const po = {
+  init() {
+
+    this.on();
+    this.off();
+
+    this.onposter();
+    this.offposter();
+
+    this.onproject();
+    this.offproject();
+  },
+
+   on() {
+    document.getElementById("overlay").style.display = "block";
+  },
+  
+  off() {
+    document.getElementById("overlay").style.display = "none";
+  },
+
+
+   onposter() {
+    document.getElementById("overlaypp").style.display = "block";
+  },
+  
+   offposter() {
+    document.getElementById("overlaypp").style.display = "none";
+  },
+
+
+   onproject() {
+    document.getElementById("overlayproject").style.display = "block";
+  },
+  
+  offproject() {
+    document.getElementById("overlayproject").style.display = "none";
+  },
+
+};
+
+portfolio.init();
+
+
+
+
+
+
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
+
+function onposter() {
+  document.getElementById("overlaypp").style.display = "block";
+}
+
+function offposter() {
+  document.getElementById("overlaypp").style.display = "none";
+}
+
+function onproject() {
+  document.getElementById("overlayproject").style.display = "block";
+}
+
+function offproject() {
+  document.getElementById("overlayproject").style.display = "none";
+}
+
+
+function myFunction() {
+var dots = document.getElementById("dots");
+var moreText = document.getElementById("more");
+var btnText = document.getElementById("btnprojects");
+
+if (dots.style.display === "none") {
+dots.style.display = "inline";
+btnText.innerHTML = "show more"; 
+moreText.style.display = "none";
+} else {
+dots.style.display = "none";
+btnText.innerHTML = "show less"; 
+moreText.style.display = "inline";
+}
+}
